0f6d982bec7efbd405663e7331a957c685d2f371
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-04-13 12:04:20 +0200

    Update store.jsx

--- src/redax/store.jsx
+++ src/redax/store.jsx
@@ -1,25 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-const persistConfig = {
-  key: 'stateContacts',
-  storage,
-  blacklist: ['filter'],
-};
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
-
-export const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['contacts/fetchContacts/fulfilled'],
+      },
+    }),
 });
-
-export const persistor = persistStore(store);
